9061bf610ab54ad0306fd822c112fff5bdc8cf13
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-10-22 07:50:55 +0100

    First working version of importer

--- R/importer.R
+++ R/importer.R
@@ -1,14 +1,20 @@
-library(XML) #might need libxml2-dev via apt-get command
+#' @export
+import_nexis <- function(path, paragraph_separator = '|'){
 
-import_nexis <- function(dir, sep = '|'){
-
-    file <- list.files(dir, full.names = TRUE, recursive = TRUE)
-    data <- data.frame()
-    for(f in file){
-        #print(file)
-        if(stri_detect_regex(f, '\\.html$|\\.htm$|\\.xhtml$', ignore.case = TRUE)){
-            data <- rbind(data, import_html(f, sep))
+    if (dir.exists(path)) {
+        dir <- path
+        file <- list.files(dir, full.names = TRUE, recursive = TRUE)
+        data <- data.frame()
+        for(f in file){
+            #print(file)
+            if(stri_detect_regex(f, '\\.html$|\\.htm$|\\.xhtml$', ignore.case = TRUE)){
+                data <- rbind(data, import_html(f, paragraph_separator))
+            }
         }
+    } else if (file.exists(path)) {
+        data <- import_html(path, paragraph_separator)
+    } else {
+        stop(path, "does not exist")
     }
     return(data)
 }
@@ -18,24 +24,13 @@ import_html <- function(file, sep = ' '){
     #Convert format
     cat('Reading', file, '\n')
 
-    # HTML cleaning------------------------------------------------
-
     line <- readLines(file, encoding = "UTF-8")
     html <- paste0(fix_html(line), collapse = "\n")
 
-    # Write to debug
-    #cat(html, file="converted.html", sep="", append=FALSE)
-
-    # Main process------------------------------------------------
-
-
-    data <- data.frame()
-
     #Load as DOM object
     dom <- htmlParse(html, encoding = "UTF-8")
+    data <- data.frame()
     for(doc in getNodeSet(dom, '//doc')){
-
-        i <- 1
         data <- rbind(data, extract_attrs(doc))
     }
     colnames(data) <- c('pub', 'edition', 'date', 'byline', 'length', 'section', 'head', 'body')
@@ -63,24 +58,22 @@ extract_attrs <- function(node, sep = "|") {
         n <- stri_length(s);
         if (is.na(n)) next
 
-        cat('----------------\n')
-        cat(i, stri_trim(s), "\n")
+        #cat('----------------\n')
+        #cat(i, stri_trim(s), "\n")
 
-        if (length(getNodeSet(div, './/span')) == 1) {
-            if (i == 2) {
-                attrs$pub <- stri_trim(s)
-            } else if (i == 3) {
-                m <- stri_match_first_regex(s, regex)
-                if (all(!is.na(m[1,2:4]))) {
-                    attrs$date <- format(as.Date(paste0(m[1,2:4], collapse = ''), '%B %d %Y'), '%Y-%m-%d')
-                }
-                if (!is.na(m[1,8])) {
-                    attrs$edition <- stri_trim(m[1,8])
-                }
-            } else if (i == 4) {
-                attrs$head <- stri_trim(s)
+        if (i == 2) {
+            attrs$pub <- stri_trim(s)
+        } else if (i == 3) {
+            m <- stri_match_first_regex(s, regex)
+            if (all(!is.na(m[1,2:4]))) {
+                attrs$date <- format(as.Date(paste0(m[1,2:4], collapse = ' '), '%B %d %Y'), '%Y-%m-%d')
             }
-        } else {
+            if (!is.na(m[1,8])) {
+                attrs$edition <- stri_trim(m[1,8])
+            }
+        } else if (i == 4) {
+            attrs$head <- stri_trim(s)
+        } else if (i >= 5) {
             if (stri_detect_regex(s, "^BYLINE: ")) {
                 attrs$byline = stri_trim(stri_replace_first_regex(s, "^BYLINE: ", ''))
             } else if (stri_detect_regex(s, "^SECTION: ")) {
@@ -92,20 +85,17 @@ extract_attrs <- function(node, sep = "|") {
                 ps <- getNodeSet(div, './/p')
                 p <- sapply(ps, xmlValue)
                 attrs$body <- stri_trim(paste0(p, collapse = paste0(' ', sep, ' ')))
-
                 n_max = n
             }
         }
         i <- i + 1
     }
+    if (attrs$pub[1] == '' || is.na(attrs$pub[1])) warning('Failed to extract publication name')
+    if (attrs$date[1] == '' || is.na(attrs$date[1])) warning('Failed to extract date')
+    if (attrs$head[1] == '' || is.na(attrs$head[1])) warning('Failed to extract heading')
+    if (attrs$body[1] == '' || is.na(attrs$body[1])) warning('Failed to extract body text')
     return(as.data.frame(attrs, stringsAsFactors = FALSE))
 }
-
-out <- import_html('/home/kohei/packages/Nexis/tests/html/irish-times_1995-06-12_0001.html')
-out <- import_html('/home/kohei/packages/Nexis/tests/html//afp_2013-03-12_0501.html')
-#out <- import_nexis('/home/kohei/packages/Nexis/tests/html/')
-print(out$date)
-
 
 clean_text <- function(str) {
     str = stri_replace_all_regex(str, '/[[:^print:]]/', ' '); # This works better
